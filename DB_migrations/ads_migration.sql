8a909c04ffec31d76fc5da4225d1d371a174c332
Travis Payne <thpaggie910@gmail.com>
2017-10-20 11:45:29 -0500

    Create ads migration to create the ads table

---
+++ DB_migrations/ads_migration.sql
@@ -0,0 +1,11 @@
+USE adlister_db;
+
+CREATE TABLE IF NOT EXISTS ads (
+  id          BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  user_id     BIGINT UNSIGNED NOT NULL,
+  title       VARCHAR(40)     NOT NULL,
+  description TEXT            NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users (id)
+    ON DELETE CASCADE
+);
