96059fc3f8264857882208a07957a36e84511201
Travis Payne <thpaggie910@gmail.com>
2017-10-23 16:11:38 -0500

    Add a Registration page and backend logic to allow a user to create an account

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -1,19 +1,47 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.models.User;
+import com.codeup.adlister.dao.DaoFactory;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(name = "controllers.RegisterServlet", urlPatterns = "/register")
 public class RegisterServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+    protected void doGet(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws ServletException, IOException {
         // TODO: show the registration form
+        request.getRequestDispatcher("WEB-INF/register.jsp").forward(request, response);
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+    protected void doPost(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws ServletException, IOException {
+
         // TODO: ensure the submitted information is valid
         // TODO: create a new user based off of the submitted information
         // TODO: if a user was successfully created, send them to their profile
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String email = request.getParameter("email");
+
+        User previouslyRegisteredUser = DaoFactory.getUsersDao().findByUsername(username);
+
+        if(previouslyRegisteredUser == null) {
+            User newUser = new User(username, email, password);
+
+            newUser.setId(DaoFactory.getUsersDao().insert(newUser));
+            request.getSession().setAttribute("user", newUser);
+            response.sendRedirect("/profile");
+        } else {
+            response.sendRedirect("/login");
+        }
     }
 }
--- src/main/webapp/WEB-INF/register.jsp
+++ src/main/webapp/WEB-INF/register.jsp
@@ -1,16 +1,29 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: T-Bomb
-  Date: 10/23/17
-  Time: 2:18 PM
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>$Title$</title>
+    <jsp:include page="partials/head.jsp">
+        <jsp:param name="title" value="Register for Account" />
+    </jsp:include>
 </head>
 <body>
-$END$
+    <%@include file="partials/navbar.jsp" %>
+    <div class="container">
+        <h2>Register For An Account</h2>
+        <form action="/register" method="post">
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="text" id="email" name="email" class="form-control"></div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-primary btn-block">Register</button>
+            <span>Already have an account? <a href="/login">Log in</a> here.</span>
+        </form>
+    </div>
 </body>
 </html>
