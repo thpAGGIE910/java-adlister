8c69ffb627e2bac38e07900cb654c98c7321b9dc
Travis Payne <thpaggie910@gmail.com>
2017-10-23 16:16:39 -0500

    Refactor Login process to read user credentials from the adlister_db database

--- src/main/java/com/codeup/adlister/controllers/LoginServlet.java
+++ src/main/java/com/codeup/adlister/controllers/LoginServlet.java
@@ -7,6 +7,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.User;
+
 @WebServlet(name = "controllers.LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -24,11 +27,12 @@ public class LoginServlet extends HttpServlet {
         // TODO: find a record in your database that matches the submitted password
         // TODO: make sure we find a user with that username
         // TODO: check the submitted password against what you have in your database
-        boolean validAttempt = false;
+        User user = DaoFactory.getUsersDao().findByUsername(username);
+        boolean validAttempt = (user != null && user.getPassword().equals(password));
 
         if (validAttempt) {
             // TODO: store the logged in user object in the session, instead of just the username
-            request.getSession().setAttribute("user", username);
+            request.getSession().setAttribute("user", user);
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
--- src/main/webapp/WEB-INF/login.jsp
+++ src/main/webapp/WEB-INF/login.jsp
@@ -19,6 +19,7 @@
                 <input id="password" name="password" class="form-control" type="password">
             </div>
             <input type="submit" class="btn btn-primary btn-block" value="Log In">
+            <span>Don't have an account? <a href="/register">Sign up</a> here.</span>
         </form>
     </div>
 </body>
